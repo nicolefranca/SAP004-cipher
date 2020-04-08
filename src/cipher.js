2fb7fc2cf1c59f59c1dd59d8941b0cf8a4f89bd5
Nicole <nicole.franca.goncalves@gmail.com>
2020-04-08 19:07:10 -0300

    Cifrando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+    encode: function encode(offset, mensagem) {
+      let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let count = mensagem.length;
+      let result = ""
+      for(var i=0; i< count ;i++){
+        let codWord = mensagem.charAt(i)
+        //  console.log(codWord)
+        let lugaralfa = alfabeto.indexOf(codWord)
+        // console.log(lugaralfa)
+        let descover = (lugaralfa + Number(offset))%26
+        // console.log(descover)
+        let wordCode = alfabeto.charAt(descover)
+        // console.log(wordCode)
+        result += wordCode
+      } 
+      return result
+    }
+    ,decode: function decode (offset, mensagem){
+      let alfabeto = "ZYXWUVTSRQPONMLKJIHGFEDCBA";
+       let count = mensagem.length;
+       let result = ""
+     for(var i=0; i< count ;i++){
+        let codWord = mensagem.charAt(i)
+        //  console.log(codWord)
+        let lugaralfa = alfabeto.indexOf(codWord)
+        // console.log(lugaralfa)
+         let descover = (lugaralfa + Number(offset))%26
+         // console.log(descover)
+         let wordCode = alfabeto.charAt(descover)
+         // console.log(wordCode)
+         result += wordCode
+       }
+       return result
+    }
+
+    
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// console.log(cipher);
+
+  // Definição de variáveis e de eventos
+   let cifra = document.getElementById("btnCode")
+   let decifra = document.getElementById("btnDecode")
+
+  // Atribuindo eventos 
+    cifra.addEventListener("click", printCode)
+     decifra.addEventListener("click", printDecode)
+
+   // Funções
+  function printCode() {
+    let mensagem = document.getElementById("word").value
+    let offset = document.getElementById("desl").value 
+    document.getElementById("finalWord").innerHTML = `Palavra cifrada: ${cipher.encode(offset,mensagem)}`
+    
+  }
+  function printDecode() {
+    let mensagem = document.getElementById("word").value
+    let offset = document.getElementById("desl").value 
+    document.getElementById("finalWord").innerHTML = `Palavra decifrada: ${cipher.decode(offset,mensagem)}`
+    
+  }
+  
+  // if(mensagem, offset === null){
+    // alert("Por favor, insira um numero ou uma palavra")
+  // }
