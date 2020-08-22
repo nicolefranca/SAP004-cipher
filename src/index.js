fa849b8fda2d03951ef2576433d3a84cdd30f975
Nicole <nicole.franca.goncalves@gmail.com>
2020-08-22 00:00:38 -0300

    Refatoreção do código e alterações no css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
- console.log(cipher);
-  // Definição de variáveis e de eventos
-   let cifra = document.getElementById("btnCode")
-   let decifra = document.getElementById("btnDecode")
-  // Atribuindo eventos 
-    cifra.addEventListener("click", printCode)
-     decifra.addEventListener("click", printDecode)
-   // Funções
-  function printCode() {
-    let mensagem = document.getElementById("word").value
-    let offset = document.getElementById("desl").value 
-    document.getElementById("finalWord").innerHTML = `Palavra cifrada: ${cipher.encode(offset,mensagem)}`
-  }
-  function printDecode() {
-    let mensagem = document.getElementById("word").value
-    let offset = document.getElementById("desl").value 
-      document.getElementById("finalWord").innerHTML = `Palavra decifrada: ${cipher.decode(offset,mensagem)}`
-  }
+// Definição de variáveis e de eventos
+const cifra = document.getElementById("btn-code");
+const decifra = document.getElementById("btn-decode");
+// Atribuindo eventos
+cifra.addEventListener("click", printCode);
+decifra.addEventListener("click", printDecode);
+// Funções
+function printCode() {
+  let mensagem = document.getElementById("word").value;
+  let offset = document.getElementById("desl").value;
+  document.getElementById("finalWord").textContent = `Palavra cifrada: ${cipher.encode(offset, mensagem)}`;
+}
+function printDecode() {
+  let mensagem = document.getElementById("word").value;
+  let offset = document.getElementById("desl").value;
+  document.getElementById(
+    "finalWord"
+  ).textContent = `Palavra decifrada: ${cipher.decode(offset, mensagem)}`;
+}
